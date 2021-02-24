884d923c1c77432829a38ea0b2165056dfb9150c
talzherz <68757705+talzherz@users.noreply.github.com>
2021-02-23 19:06:31 -0500

    slowing down scroll

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $('.scroll').click(function(){
    console.log("Your index.js file is loaded correctly!");
   $("html, body").animate(
       {scrollTop: $("#Row1").offset().top},
-      400,
+      500,
       "swing",
       function(){
          }
